a4a5041b0ebec5beac9e66546837f8c2b0b22b16
Wilbert Padron <wilbertjose10@gmail.com>
2024-08-19 13:02:33 -0600

    fix

--- app/xvjuan2024/layout.tsx
+++ app/xvjuan2024/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 export const metadata: Metadata = {
   title: "XV Años - Juan Roberto",
   metadataBase: new URL("https://padga.dev"),
   openGraph: {
     description: "Invitación digital de la celebración",
+    images: ["/img/xv6-r.jpg"],
   },
+  icons: ["/img/xv6-r.jpg"],
 };
 
 export default function XVJuanLayout({
@@ -15,12 +16,6 @@ export default function XVJuanLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          property="og:image"
-          content="https://padga.dev/img/xv6-r.jpg"
-        />
-      </Head>
       <body>{children}</body>
     </html>
   );
