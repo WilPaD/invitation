2cbe1bc001020e29021d2cbe5c0c1c7ad0707d3f
Wilbert Padron <wilbertjose10@gmail.com>
2024-08-19 10:24:55 -0600

    z3

--- app/xvjuan2024/layout.tsx
+++ app/xvjuan2024/layout.tsx
@@ -4,9 +4,9 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://padga.dev"),
   openGraph:{
 	description: "Invitación digital de la celebración",
-	images: ['/img/xv6.jpg'],
+	images: ['/img/xv6-r.jpg'],
   },
-  icons: ["/img/xv6.jpg"],
+  icons: ["/img/xv6-r.jpg"],
 };
 
 export default function XVJuanLayout({
